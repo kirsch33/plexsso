c71c8696d55e51293129f9265705830e3bc162e1
akirsch <atki224@g.uky.edu>
2020-11-27 22:15:54 -0500

    redo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kirsch33/plexsso
+
+go 1.15
+
+require github.com/caddyserver/caddy/v2 v2.0.0
---
+++ sso.go
@@ -0,0 +1,116 @@
+package plexsso
+
+import (
+	"fmt"
+	"net/http"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type plexsso struct {
+	plex_token string
+	logger *zap.Logger
+}
+
+type token struct {
+	plexToken string
+}
+
+func init() {
+	caddy.RegisterModule(plexsso{})
+	httpcaddyfile.RegisterHandlerDirective("plexsso", parseCaddyfileHandler)
+}
+
+func (s *plexsso) Provision(ctx caddy.Context) error {
+	s.logger = ctx.Logger(s) 
+}
+
+func (plexsso) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID: "http.handlers.plexsso",
+		New: func() caddy.Module {
+			return new(plexsso)
+		},
+	}
+}
+
+func parseCaddyfileHandler(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var s plexsso
+	err := s.UnmarshalCaddyfile(h.Dispenser)
+	if err != nil {
+		return nil, err
+	}
+	return s, err
+}
+
+func parseStringArg(d *caddyfile.Dispenser, out *string) error {
+	if !d.Args(out) {
+		return d.ArgErr()
+	}
+	return nil
+}
+
+func (s plexsso) ServeHTTP(w http.ResponseWriter, req *http.Request, handler caddyhttp.Handler) error {
+	
+	u := req.URL.String()
+	h := req.Header.Get("referer")
+	
+	if u=="https://ombi.greatwhitelab.net/" && h=="https://greatwhitelab.net/auth/portal" {
+		
+		req_body, err := json.Marshal(map[string]string{"plexToken": s.token})
+		
+		if err != nil {
+			return fmt.Errorf("Token formatting error: %s", err)
+		}
+		
+		resp, err := http.Post("http://192.168.42.12:3579/api/v1/token/plextoken", "application/json", bytes.NewBuffer(req_body))
+		
+		if err != nil {
+			return fmt.Errorf("Response error: %s", err)
+		}
+		
+		defer resp.Body.Close()
+		
+		res_body, err := ioutil.ReadAll(resp.Body)
+		
+		s.logger.Debug("kodiak debug log", zap.String("res_body",res_body))
+		
+		return handler.ServeHTTP(w, req) 
+	}
+	
+	return handler.ServeHTTP(w, req)
+}
+	
+func (s *plexsso) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	d.NextArg()
+	
+	for d.NextBlock(0) {	
+		var err error
+		
+		switch d.Val() {
+			case "token":
+				err = parseStringArg(d, &s.plex_token)
+			default:
+				return d.Errf("Unknown plexsso arg")
+			}
+			if err != nil {
+				return d.Errf("Error parsing %s: %s", d.Val(), err)
+		}
+	}
+	return nil
+}
+
+var (
+	_ caddyfile.Provision         = (*plexsso)(nil)
+	_ caddyhttp.MiddlewareHandler = (*plexsso)(nil)
+	_ caddyfile.Unmarshaler       = (*plexsso)(nil)
+)
